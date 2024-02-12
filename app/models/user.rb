3081fe16ff8d9f489c589d288e01971071cf90e6
naotakaoiwa <babtos.0506@gmail.com>
2024-02-12 12:21:49 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  with_options presence: true, format: { with: /\A[a-zA-Z]+\z/, message: 'is invalid. Input English letters' } do
+    validates :nickname
+    validates :gender
+  end
+  with_options presence: true, format: { with: /\A[0-9０-９]+\z/, message: 'is invalid. Input numbers' } do
+    validates :age
+  end
 end
